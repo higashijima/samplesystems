c8c98e6787b8767a6835424a6b7e5933f83649f6
m-higashijima <boctok.ctpoba@gmail.com>
2017-03-22 00:02:20 +0900

    CSVとPDFのアダプタ追加

---
+++ samplesystem/src/jp/example/samplesystem/dataproc/csv/CsvAdapter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package jp.example.samplesystem.dataproc.csv;
+
+import jp.example.samplesystem.dataproc.common.AbstractBean;
+import jp.example.samplesystem.dataproc.common.AbstractProc;
+
+/**
+ * @author M.H
+ *
+ */
+public class CsvAdapter extends AbstractProc {
+	
+	/* (非 Javadoc)
+	 * @see jp.example.samplesystem.dataproc.common.AbstractProc#output(jp.example.samplesystem.dataproc.common.AbstractBean)
+	 */
+	@Override
+	protected void output(AbstractBean bean) {
+		// beanデータからCSVを作る
+		
+	}
+	
+}
---
+++ samplesystem/src/jp/example/samplesystem/dataproc/pdf/PdfAdapter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package jp.example.samplesystem.dataproc.pdf;
+
+import jp.example.samplesystem.dataproc.common.AbstractBean;
+import jp.example.samplesystem.dataproc.common.AbstractProc;
+
+/**
+ * @author M.H
+ *
+ */
+public class PdfAdapter extends AbstractProc {
+	
+	/* (非 Javadoc)
+	 * @see jp.example.samplesystem.dataproc.common.AbstractProc#output(jp.example.samplesystem.dataproc.common.AbstractBean)
+	 */
+	@Override
+	protected void output(AbstractBean bean) {
+		// TbeanデータからPDFを作る
+		
+	}
+	
+}
