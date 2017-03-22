7e968015377636750db25e15381817c412438b7f
m-higashijima <boctok.ctpoba@gmail.com>
2017-03-22 21:55:41 +0900

    アノテーションターゲットをFIELDに修正

--- samplesystem/src/jp/example/samplesystem/dataproc/common/DataFormat.java
+++ samplesystem/src/jp/example/samplesystem/dataproc/common/DataFormat.java
@@ -1,13 +1,13 @@
 package jp.example.samplesystem.dataproc.common;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target(FIELD)
 /**
  * データの書式を設定するアノテーション
  * @author M.H
